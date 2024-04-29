95033e9aab03858e4e0114ae1c799394326b4fc1
Ricky Kirkham <rickki@microsoft.com>
2024-04-29 14:04:49 -0700

    moving content from the shared-runtime branch of the CF TypeScript repo

---
+++ test/end-to-end/webpack.config.js
@@ -0,0 +1,101 @@
+/* eslint-disable no-undef */
+
+const devCerts = require("office-addin-dev-certs");
+const CustomFunctionsMetadataPlugin = require("custom-functions-metadata-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path");
+
+/* global require, module, process, __dirname */
+
+async function getHttpsOptions() {
+  const httpsOptions = await devCerts.getHttpsServerOptions();
+  return { ca: httpsOptions.ca, key: httpsOptions.key, cert: httpsOptions.cert };
+}
+
+module.exports = async (env, options) => {
+  const debuggingTest = env.testType === "debugger";
+  const config = {
+    devtool: "source-map",
+    entry: {
+      polyfill: ["core-js/stable", "regenerator-runtime/runtime"],
+      commands: "./src/commands/commands.ts",
+      taskpane: ["./test/end-to-end/src/test-taskpane.ts", "./test/end-to-end/src/test-taskpane.html"],
+      functions: "./src/functions/functions.ts",
+    },
+    output: {
+      path: path.resolve(__dirname, "testBuild"),
+      clean: true,
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".html", ".js"],
+      fallback: {
+        child_process: false,
+        fs: false,
+        os: require.resolve("os-browserify/browser"),
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-typescript"],
+            },
+          },
+        },
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: "ts-loader",
+        },
+        {
+          test: /\.html$/,
+          exclude: /node_modules/,
+          use: "html-loader",
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif|ico)$/,
+          type: "asset/resource",
+          generator: {
+            filename: "assets/[name][ext][query]",
+          },
+        },
+      ],
+    },
+    plugins: [
+      new CustomFunctionsMetadataPlugin({
+        output: "functions.json",
+        input: "./src/functions/functions.ts",
+      }),
+      new HtmlWebpackPlugin({
+        filename: "taskpane.html",
+        template: "./test/end-to-end/src/test-taskpane.html",
+        chunks: ["polyfill", "taskpane", "functions", "commands"],
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: "assets/*",
+            to: "assets/[name][ext][query]",
+          },
+        ],
+      }),
+    ],
+    devServer: {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      server: {
+        type: "https",
+        options: env.WEBPACK_BUILD || options.https !== undefined ? options.https : await getHttpsOptions(),
+      },
+      port: debuggingTest ? 3001 : process.env.npm_package_config_dev_server_port || 3000,
+    },
+  };
+
+  return config;
+};
